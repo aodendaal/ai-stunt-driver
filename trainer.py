409f14bce9ef4e33e6c3e76defec74e074ecd7e7
Andre Odendaal <aodendaal@gmail.com>
2019-07-19 10:44:32 +0200

    Use batching when reading training data

--- trainer.py
+++ trainer.py
@@ -1,11 +1,7 @@
 import tensorflow as tf
 import cnn_model
 import training_data
-
-print('loading training data')
-data, labels = training_data.get_data_and_labels()
-print('finished loading training data')
-print('records found: {0}'.format(len(data)))
+import numpy as np
 
 with tf.Session() as session:
     x = tf.placeholder(tf.float32, shape=[None, training_data.width * training_data.height])
@@ -16,12 +12,27 @@ with tf.Session() as session:
     init = tf.global_variables_initializer()
     session.run(init)
 
-    session.run(model.optimize, feed_dict={x: data, y: labels})
+    data_total = training_data.get_file_size()
+    count = 0
 
+    while True:
+        batch_size = np.minimum(data_total, 100)
+        data_total = data_total - batch_size
+        print('Processing batch {0} - {1} rows remaining'.format(count, data_total))
+
+        data, labels = training_data.get_batch(batch_size)
+        session.run(model.optimize, feed_dict={x: data, y: labels})
+
+        count = count + 1
+
+        if (data_total == 0):
+            break
+
+    print('Testing accuracy')
     test_data, test_labels = training_data.get_test_data_and_labels()
 
     result = session.run(model.error, feed_dict={x: test_data, y: test_labels})
-    print("Accuracy: {0}%".format(int(result * 100)))
+    print('Accuracy: {0}%'.format(int(result * 100)))
 
     saver = tf.train.Saver()
-    save_path = saver.save(session, "tmp/cnn.ckpt")
+    save_path = saver.save(session, 'tmp/cnn.ckpt')
--- training_data.py
+++ training_data.py
@@ -7,9 +7,26 @@ width = int(640 * resize_percentage)
 height = int(400 * resize_percentage)
 channels = 9
 
+skip_count = 0
+recording_filename = "data/recording.csv"
+test_filename = "data/test.csv"
 
-def get_data_and_labels():
-    df = pd.read_csv("data/recording.csv")
+
+def get_file_size():
+    global recording_filename
+
+    with open(recording_filename) as f:
+        size = sum(1 for line in f)
+
+    return size
+
+
+def get_batch(count):
+    global skip_count
+    global recording_filename
+
+    df = pd.read_csv(recording_filename, skiprows=skip_count, nrows=count)
+    skip_count = skip_count + count
     data = df.to_numpy()
     raw_data = list(map(lambda row: ast.literal_eval(row[0]), data))
     training_data = list(map(lambda row: list(map(lambda cell: cell / 255, row)), raw_data))
@@ -20,7 +37,9 @@ def get_data_and_labels():
 
 
 def get_test_data_and_labels():
-    df = pd.read_csv("data/test.csv")
+    global test_filename
+
+    df = pd.read_csv(test_filename)
     data = df.to_numpy()
     raw_data = list(map(lambda row: ast.literal_eval(row[0]), data))
     training_data = list(map(lambda row: list(map(lambda cell: cell / 255, row)), raw_data))
